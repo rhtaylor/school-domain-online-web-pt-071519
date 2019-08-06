0d8863d70244ae20cef762097f70952e0eb5e298
Randy Taylor <randyisbusy@gmail.com>
2019-08-06 20:24:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+
+  def sort
+  nu = {}
+  roster.each do |grade, student|
+    nu[grade] = student.sort
+    nu.sort
+  end
+  nu.keys.each do |key|
+    key.to_sym
+  binding.pry
+  end
+  binding.pry
+end
+end
